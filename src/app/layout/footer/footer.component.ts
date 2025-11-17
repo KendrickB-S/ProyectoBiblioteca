e17d07ea35743e2fb836819458954dbd588861f4
KendrickB-S <kendrik.23.bs@gmail.com>
2025-11-16 21:32:21 -0500

    footer actualizado

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'; // <-- ¡Importante!
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink], // <-- ¡Añádelo aquí!
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css',
+  styleUrl: './footer.component.css'
 })
-export class Footer {
+export class FooterComponent {
 
 }

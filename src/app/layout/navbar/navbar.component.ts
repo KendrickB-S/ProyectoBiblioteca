54d4a059a4cb66709d31ff6c4de062f290e0a1df
KendrickB-S <kendrik.23.bs@gmail.com>
2025-11-16 21:32:45 -0500

    navbar actualizado

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router'; // <-- ¡Importante!
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink], // <-- ¡Asegúrate de importar RouterLink aquí!
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+  styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 

912fe5b9916d2b51f6ae55c04a162c2047ce7f24
KendrickB-S <kendrik.23.bs@gmail.com>
2025-11-19 00:12:10 -0500

    catalogo conectado con el backend

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -1,28 +1,28 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // ¡Importante para *ngFor!
+import { Component, OnInit, inject, signal } from '@angular/core'; 
+import { CommonModule } from '@angular/common';
+import { LibroService, Libro } from '../../services/libro.service';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [CommonModule], // <-- ¡Añade CommonModule!
+  imports: [CommonModule],
   templateUrl: './catalogo.component.html',
-  styleUrl: './catalogo.component.css' // <-- ¡Apunta a tu nuevo CSS!
+  styleUrl: './catalogo.component.css'
 })
-export class CatalogoComponent {
+export class CatalogoComponent implements OnInit {
+  
+  private libroService = inject(LibroService);
+  libros = signal<Libro[]>([]); 
 
-  // Por ahora, definimos los libros aquí.
-  // Más adelante, esto vendrá de tu API de Spring Boot.
-  libros = [
-    { titulo: 'Orgullo y prejuicio', autor: 'Jane Austen', portadaUrl: '/img/libro1.jpg' },
-    { titulo: 'El Principito', autor: 'Antoine de Saint-Exupéry', portadaUrl: '/img/libro2.jpg' },
-    { titulo: 'La llamada de lo salvaje', autor: 'Jack London', portadaUrl: '/img/libro3.jpg' },
-    { titulo: 'La sombra del viento', autor: 'Carlos Ruiz Zafón', portadaUrl: '/img/libro4.jpg' },
-    { titulo: 'La delicadeza', autor: 'David Foenkinos', portadaUrl: '/img/libro5.jpg' },
-    { titulo: '1984', autor: 'George Orwell', portadaUrl: '/img/libro6.jpg' },
-    { titulo: 'La casa de los espíritus', autor: 'Isabel Allende', portadaUrl: '/img/libro7.jpg' },
-    { titulo: 'El baile', autor: 'Irène Némirovsky', portadaUrl: '/img/libro8.jpg' },
-    { titulo: 'La casa de la alegría', autor: 'Edith Wharton', portadaUrl: '/img/libro9.jpg' },
-    { titulo: 'Veinte poemas de amor...', autor: 'Pablo Neruda', portadaUrl: '/img/libro10.jpg' }
-  ];
-
+  ngOnInit(): void {
+    this.libroService.obtenerLibros().subscribe({
+      next: (datos) => {
+        this.libros.set(datos);
+        console.log('Libros cargados:', this.libros()); 
+      },
+      error: (err) => {
+        console.error('Error al cargar libros:', err);
+      }
+    });
+  }
 }

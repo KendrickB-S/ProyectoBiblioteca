d1213565543312ccaddced5af01b50396a6bfa93
KendrickB-S <kendrik.23.bs@gmail.com>
2025-11-19 00:09:55 -0500

    pagina para el admin

---
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-admin-layout',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './admin-layout.component.html',
+  styleUrl: './admin-layout.component.css'
+})
+export class AdminLayoutComponent {
+  authService = inject(AuthService);
+
+  logout() {
+    this.authService.logout();
+    window.location.href = '/login'; 
+  }
+}

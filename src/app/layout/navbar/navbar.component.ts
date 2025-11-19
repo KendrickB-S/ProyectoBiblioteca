180261d222447646e2d0e344ee9cb99f81f20904
KendrickB-S <kendrik.23.bs@gmail.com>
2025-11-19 00:10:21 -0500

    cambios para separar al admin

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'; // <-- ¡Importante!
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink], // <-- ¡Asegúrate de importar RouterLink aquí!
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  authService = inject(AuthService); 
 
+  logout() {
+    this.authService.logout();
+  }
 }
